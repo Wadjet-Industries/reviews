c04b9cf001c253dc5f1e5f0e14fbb942fdacd4b7
Pamela Tung <pammyla@pamelas-air.lan>
2019-08-22 23:53:14 -0700

    added webpack and server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
+
