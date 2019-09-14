383a2d3ecbbb048a902e1e2438ab72a369cc541c
Jessica Yu <jessyu2010@gmail.com>
2019-09-14 16:35:36 -0700

    Updated data to seed to csv for > 1m data points

---
+++ database/seedcsv-reviews.js
@@ -0,0 +1,65 @@
+var csvWriter = require('csv-write-stream')
+const fs = require('fs');
+const faker = require('faker');
+var writer = csvWriter();
+
+const createFile = fs.createWriteStream('reviews100.csv');
+createFile.write('user_id,review,overall,food,service,ambience,value,noise,would_recommend,date,restaurant_id\n', 'utf-8');
+
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const seedReviews = (writer, encoding, callback) => {
+  let noiseLevel = ['Quiet', 'Moderate', 'Energetic'];
+  let i = 100000000;
+  function write() {
+    let ok = true;
+    do {
+      i--;
+      let randomUserID =  getRandomIntInclusive(1, 300);
+      let randomReview = faker.lorem.paragraph();
+      let randomFood = getRandomIntInclusive(1, 5);
+      let randomService = getRandomIntInclusive(1, 5);
+      let randomAmbience = getRandomIntInclusive(1, 5);
+      let randomValue = getRandomIntInclusive(1, 5);
+      if(randomFood >=4) {
+        randomService = getRandomIntInclusive(4, 5);
+        randomAmbience = getRandomIntInclusive(4, 5);
+        randomValue = getRandomIntInclusive(4, 5);
+      }
+      let overallAverage = (randomFood + randomService +randomAmbience + randomValue) / 4;
+      let randomNoise = noiseLevel[getRandomIntInclusive(0, 2)];
+      let recommend = faker.random.boolean();
+      if (overallAverage >=4) {
+        recommend = 1;
+      }
+      let randomDate = faker.date.past();
+      if (getRandomIntInclusive(1, 10) > 7) {
+        randomDate = faker.date.recent();
+      }
+      let randomRestaurantID = getRandomIntInclusive(1, 100);
+      let data = `${randomUserID},'${randomReview}',${overallAverage},${randomFood},${randomService},${randomAmbience},${randomValue},'${randomNoise}',${recommend},'${randomDate}',${randomRestaurantID}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+      if (i % 10000 === 0) {
+        let now = new Date();
+        let timeNow = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        console.log(`${timeNow} Wrote user ${(100000000 - i).toLocaleString()}`);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  }
+  write();
+} 
+
+seedReviews(createFile, 'utf-8', () => { writer.end(); });
+
+
--- database/seedcsv-users.js
+++ database/seedcsv-users.js
@@ -9,40 +9,50 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const seedUsers = () => {
-  writer.pipe(fs.createWriteStream('users.csv'));
-  let background = ['#BB6ACD', '#D86441', '#6C8AE4', '#DF4E96'];
-  for (var i = 1; i <= 10000000; i++) {
-    let randomUser =  '\'' + faker.name.firstName();
-    if (getRandomIntInclusive(1, 10) > 3) {
-      randomUser += ' ' + faker.name.lastName() + '\'';
-    }
-    let randomInitialsBG = background[getRandomIntInclusive(0, 3)];
-    let randomLocation = `'${faker.address.city()}'`;
-    let vipStatus = false;
-    if (getRandomIntInclusive(1, 10) > 7) {
-      vipStatus = true;
-    }
-    let randomTotalReviews = getRandomIntInclusive(1, 30);
-    if (getRandomIntInclusive(1, 10) > 7) {
-      randomTotalReviews = getRandomIntInclusive(31, 200);
-    }
-    writer.write({
-      user: randomUser, 
-      initials_background: `'${randomInitialsBG}'`,
-      location: randomLocation, 
-      vip: vipStatus,
-      total_reviews: randomTotalReviews
-    });
-    if (i % 10000 === 0) {
-      console.log(`Wrote user #${i}`);
-    }
-  };
-  console.log('End users seeding');
-  writer.end();
-}
+const createFile = fs.createWriteStream('users.csv');
+createFile.write('user,initials_background,location,vip,total_reviews\n', 'utf-8');
 
-seedUsers();
+
+const seedUsers = (writer, encoding, callback) => {
+  let background = ['#BB6ACD', '#D86441', '#6C8AE4', '#DF4E96'];
+  let i = 10000000;
+  function write() {
+    let ok = true;
+    do {
+      i--;
+      let randomUser =  faker.name.firstName();
+      if (getRandomIntInclusive(1, 10) > 3) {
+        randomUser += ' ' + faker.name.lastName();
+      }
+      let randomInitialsBG = background[getRandomIntInclusive(0, 3)];
+      let randomLocation = faker.address.city();
+      let vipStatus = false;
+      if (getRandomIntInclusive(1, 10) > 7) {
+        vipStatus = true;
+      }
+      let randomTotalReviews = getRandomIntInclusive(1, 30);
+      if (getRandomIntInclusive(1, 10) > 7) {
+        randomTotalReviews = getRandomIntInclusive(31, 200);
+      }
+      let data = `'${randomUser}','${randomInitialsBG}','${randomLocation}',${vipStatus},${randomTotalReviews}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+      if (i % 10000 === 0) {
+        let now = new Date();
+        let timeNow = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        console.log(`${timeNow} Wrote user ${(10000000 - i).toLocaleString()}`);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  }
+  write();
+}
+seedUsers(createFile, 'utf-8', () => { writer.end(); });
 // seedReviews();
 
 
