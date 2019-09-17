f72978fbfd7864daa918419221e1c8c2230704d0
Jessica Yu <jessyu2010@gmail.com>
2019-09-17 16:10:25 -0700

    Updated listing csv data degenerator to add restaurant name

--- database/seedcsv-listings.js
+++ database/seedcsv-listings.js
@@ -3,24 +3,37 @@ const fs = require('fs');
 const faker = require('faker');
 var writer = csvWriter();
 
-const getRandomIntInclusive = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const createFile = fs.createWriteStream('listing.csv');
+writer.write('id,restaurant\n','utf-8');
 
 const seedRestaurants = () => {
-  writer.pipe(fs.createWriteStream('listing.csv'));
-  for (var i = 1; i <= 10000000; i++) {
-    writer.write({ listing: i });
+  let i = 10000000;
+  function write() {
+    do {
+      i--;
+      let name = faker.lorem.word();
+      let data = i + ',' + name + '\n';
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+      if (i % 100000 === 0) {
+        let now = new Date();
+        let timeNow = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        console.log(`${timeNow} Wrote user ${i.toLocaleString()}`);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
   }
-  console.log('End of seed restaurants');
-  writer.end();
+  write();
 };
 
 
-seedRestaurants();
-// seedUsers();
-// seedReviews();
-
+seedRestaurants(createFile, 'utf-8', () => {
+  writer.end();
+});
+console.log('End of seed restaurants');
 
